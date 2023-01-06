02e136b5919732b867084b13ca66b6fd22b6c185
Kavinkumar1998 <kavinajith412@gmail.com>
2023-01-06 21:54:14 +0530

    git commit

--- script.js
+++ script.js
@@ -2,56 +2,42 @@ var row = document.querySelector(".row");
 fetch("https://restcountries.com/v3.1/all")
 .then((response)=>response.json())
 .then((data)=>{
-    data.forEach(element=>{
-        const countryrequired={
-            ...element,
-            name: element.name.common,
-            flag: element.flags.png,
-            population: element.population,
-            region: element.region,
-            capital: element.capital,
-            lattitude: element.latlng[0],
-            longitude: element.latlng[1],
-            countrycode: element.cca3,
-        }
-        Country(element.name.common,element.flags.png,element.population,element.region,element.capital,element.latlng[0],element.latlng[1],element.cca3)
-       
-    });
+    for(var i=0; i<data.length;i++){
+        row.innerHTML +=`
+        <div class="col-sm-6 col-md-4 col-lg-4 col-xl-4">
+            <div class="card h-100">
+            <div class="card-header">
+         <H1 id="title" class="text-center"><span style:"color:black;">${data[i].name.common}</span></H1>
+         </div>
+         <img src="${data[i].flags.png}" class="card-img-top" alt="flag">
+         <div class="card-body">
+         <div class="card-text">
+         <div><span>Capital :</span> ${ data[i].capital}</div>
+         <div><span>Country code :</span> ${ data[i].cca3}</div>
+        <div><span>Region :</span> ${ data[i].region }</div>
+        <div><span>Population :</span> ${ data[i].population }</div>
+         <div><span>Lattitude :</span> ${ data[i].latlng[0]}</div>
+         <div><span>Longitude :</span> ${ data[i].latlng[1]}</div>
+         <div class="weather" id="index-${i}"></div>
+         <div ><button onclick="getWeather('${data[i].latlng[0]}','${data[i].latlng[1]}','index-${i}')" class="btn">click for temprature</button></div>
+         </div>
+         </div>
+         </div>
+         </div>
+            `   
+    }
 })
 .catch((err)=>console.log(err))
 
-function Country(name,flag,poplation,region,capital,lattitude,longitude,countrycode){
-row.innerHTML +=`
-<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4">
-    <div class="card">
-    <div class="card-header">
- <H1 id="title" class="text-center"><span style:"color:black;">${name}</span></H1>
- </div>
- <img src="${flag}" class="card-img-top" alt="flag">
- <div class="card-body">
- <div class="card-text">
- <div><span>Capital :</span> ${ capital}</div>
- <div><span>Country code :</span> ${ countrycode}</div>
-<div><span>Region :</span> ${ region }</div>
-<div><span>Population :</span> ${ poplation }</div>
- <div><span>Lattitude :</span> ${ lattitude}</div>
- <div><span>Longitude :</span> ${ longitude}</div>
- <div><button onclick="getWeather(${lattitude},${longitude})" class="btn">click for weather</button></div>
- </div>
- </div>
- </div>
- </div>.
-    `   
-}
-
-function getWeather(lat,lon){
-    console.log(lat,lon);
+function getWeather(lat,lon,id){
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d11682ccc7fb55ab9b2f12551fd45c26`)
        .then((response) => response.json())
        .then((data)=>{
-           document.querySelector(".btn").innerText = data.main.temp;
-           console.log(data.main.temp);
+        let weatherData= document.getElementById(id);
+        let temprature =(`Temperature: ${(parseInt(data.main.temp)- 273)} C Sky:${data.weather[0].main}`);
+           weatherData.append(temprature);
        })
 
        .catch((err)=>console.log(err))
 }
+
